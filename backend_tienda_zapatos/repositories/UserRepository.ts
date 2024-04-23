04d122d69edd8289887d3bb8d1f4a00638580b9f
meliza <melizarodriguezsoler@gmail.com>
2024-04-23 09:25:29 -0500

    Tienda Zapatos

---
+++ backend_tienda_zapatos/repositories/UserRepository.ts
@@ -0,0 +1,25 @@
+import db from "../config/config-db";
+import User from "../Dto/UserDto";
+
+class UserRepository {
+  static async add(user: User) {
+    const sql =
+      "INSERT INTO users (email, nombres, apellidos, telefono, password) VALUES (?, ?, ?, ?, ?)";
+    const values = [
+      user.email,
+      user.nombres,
+      user.apellidos,
+      user.telefono,
+      user.password,
+    ];
+    return db.execute(sql, values);
+  }
+
+  static async login(user: User) {
+    const sql = "SELECT password FROM users WHERE email=?";
+    const values = [user.email];
+    return db.execute(sql, values);
+  }
+}
+
+export default UserRepository;
